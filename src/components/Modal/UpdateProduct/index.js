9f96834652a8868275f46ed5860558c4ae90ff59
gustavocgb11@gmail.com <gustavocgb11@gmail.com>
2021-04-13 23:30:45 -0300

    input

--- src/components/Modal/UpdateProduct/index.js
+++ src/components/Modal/UpdateProduct/index.js
@@ -4,7 +4,10 @@ import * as Styles from './styles'
 import {useState} from "react";
 
 export default function ModalUpdateProduct({product, setIsModalVisible}) {
+    const [nome, setName] = useState(product?.nome)
     const [color, setColor] = useState(product?.cor)
+    const [descricao, setDesc] = useState(product?.descricao)
+    const [valor_atual, setValue] = useState(product?.valor_atual)
     const [size, setSize] = useState(product?.tamanho)
     const [image, setImage] = useState(product?.imagem)
 
@@ -13,7 +16,7 @@ export default function ModalUpdateProduct({product, setIsModalVisible}) {
 
     const onClick = async (event) => {
         event.preventDefault()
-        await api.put(`/products/${product?.codigo}?cor=${color}&tamanho=${size}&imagem=${image}`)
+        await api.put(`/products/${product?.codigo}?cor=${color}&tamanho=${size}&imagem=${image}&nome=${nome}&descricao=${descricao}&valor_atual=${valor_atual}`)
         setIsModalVisible(false)
         location.reload()
     }
@@ -23,6 +26,15 @@ export default function ModalUpdateProduct({product, setIsModalVisible}) {
             <form>
                 <h2 className="uk-modal-title">Editar produto</h2>
                 <div className="uk-margin">
+                <label class="uk-form-label"><strong>Nome</strong></label>
+                    <input name="nome"
+                           className="uk-input uk-margin-small-bottom"
+                           type="text"
+                           value={nome}
+                           onChange={event => setName(event.target.value)}
+                           placeholder="Nome"
+                           required
+                    />
                     <label class="uk-form-label"><strong>Cor</strong></label>
                     <input name="cor"
                            className="uk-input uk-margin-small-bottom"
@@ -39,6 +51,24 @@ export default function ModalUpdateProduct({product, setIsModalVisible}) {
                            value={size}
                            onChange={event => setSize(event.target.value)}
                            placeholder="Tamanho"
+                           required
+                    />
+                    <label class="uk-form-label"><strong>Descrição</strong></label>
+                    <textarea name="descricao"
+                           className="uk-input uk-margin-small-bottom"
+                           type="text"
+                           value={descricao}
+                           onChange={event => setDesc(event.target.value)}
+                           placeholder="Descrição"
+                           required
+                    />
+                    <label class="uk-form-label"><strong>Valor</strong></label>
+                    <input name="valor_atual"
+                           className="uk-input uk-margin-small-bottom"
+                           type="number"
+                           value={valor_atual}
+                           onChange={event => setValue(event.target.value)}
+                           placeholder="Valor"
                            required
                     />
                     <label class="uk-form-label"><strong>Imagem</strong></label>
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,9 +6,22 @@ export default function NavBar() {
                     <ul class="uk-navbar-nav">
                         <li class="uk-active"><a href="/">Store</a></li>
                         <li><a href="#">Produtos 1</a></li>
-                        <li><a href="#">Produtos 2</a></li>
-                    </ul>
-                </div>
+                        <li><a href="#">Produtos 2</a></li>                        
+                    </ul>    
+                    <div class="uk-navbar-right">
+                        <div class="uk-navbar-item">
+                            <form action="javascript:void(0)">
+                                <select class="uk-select uk-form-width-small" type="text" placeholder="Input">
+                                    <option value='type'>Tipo</option>
+                                    <option value='genre'>Genero</option>
+                                    <option value='type'>Tipo</option>
+                                </select>
+                                <input class="uk-input uk-form-width-small" type="text" placeholder="Input"/>
+                                <button class="uk-button uk-button-default"><span uk-icon="search" ></span></button>
+                            </form>
+                        </div>
+                    </div>              
+                </div>                
             </div>
         </nav>
     )
